70935ff5066883c9d047c4ef634f9e89e156675b
Rıdvan Çakmak <cakmak.ridvan@hotmail.com>
2019-07-11 18:15:03 +0300

    Version methods added  for api 29

--- app/src/main/java/com/rotamobile/gursan/application/MainApplication.java
+++ app/src/main/java/com/rotamobile/gursan/application/MainApplication.java
@@ -3,6 +3,7 @@ package com.rotamobile.gursan.application;
 import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
+import android.support.multidex.MultiDex;
 
 import com.rotamobile.gursan.data.prefs.LocaleHelper;
 import com.rotamobile.gursan.service.FireBaseService;
@@ -15,6 +16,7 @@ public class MainApplication extends Application {
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(LocaleHelper.onAttach(base,"tr"));
+        MultiDex.install(this);
 
     }
 
